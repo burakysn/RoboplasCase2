6b3f0c32f8ef38adf4c1c6aa42e5de1c8ddea19d
burak-yasan <burak_yasan@hotmail.com>
2023-10-29 17:53:49 +0300

    .

--- RoboplasCase2/WebAPI/Program.cs
+++ RoboplasCase2/WebAPI/Program.cs
@@ -14,7 +14,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.WithOrigins(
+            "http://localhost:4200"
+            )
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,5 +40,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors("AllowSpecificOrigin");
 
 app.Run();
